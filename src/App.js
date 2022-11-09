3a75f06a55d328accc6ce2aeb483f1266de7f0ce
Shanks Onyango <69570282+George-Onyango@users.noreply.github.com>
2022-11-09 11:21:07 +0300

    Revert "Ft password"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import Login from './components/Login'
-import ForgotPassword from './components/ForgotPassword'
 import './App.css';
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-      <Route path="/" element={<Login/>}>
-      </Route>
-
-      <Route path="/ForgotPassword" element={<ForgotPassword/>}>
-      </Route>
-      </Routes>
+      {/* <header className="App-header">
+      </header> */}
+      <Login />
     </div>
   );
 }
